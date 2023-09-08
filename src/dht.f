7bf72e1265e83ea751496c620a7781c00070c169
justdoit1897 <vincenzofardella96@gmail.com>
2023-09-08 15:20:03 +0200

    Aggiunti commenti dht.f

--- src/dht.f
+++ src/dht.f
@@ -1,80 +1,103 @@
-HEX
-20000000            CONSTANT RPI1_BASE
-RPI1_BASE 200000 +  CONSTANT GPIO_BASE
-
-GPIO_BASE           CONSTANT GPFSEL0
-GPIO_BASE 4 +       CONSTANT GPFSEL1
-
-GPIO_BASE 1C +      CONSTANT GPSET0
-GPIO_BASE 28 +      CONSTANT GPCLR0
-GPIO_BASE 34 +      CONSTANT GPLEV0
-
-RPI1_BASE 3000 +    CONSTANT SYSTEM_TIME_BASE
-SYSTEM_TIME_BASE 4 + CONSTANT CLO_REGISTER
-
-0                   CONSTANT INP
-1                   CONSTANT OUT
-
 DECIMAL
-: ABS DUP 0< IF -1 * THEN ;
-: MILLISECONDS 1000 * ;
-: DELAY CLO_REGISTER @ BEGIN 2DUP CLO_REGISTER @ - ABS SWAP > UNTIL 2DROP ;
-
-: BIC INVERT AND ;
-: BILS 1 SWAP LSHIFT ;
-
-: ENABLE_PIN DUP >R @ -ROT >R BIC R> OR R> ! ;          
-
-18 BILS             CONSTANT GPIO18
-
-: N_GPIO 0 SWAP BEGIN DUP 2 MOD 0 = IF 1 RSHIFT SWAP 1+ SWAP ELSE THEN DUP 2 = UNTIL DROP 1+ ;
-: MODE SWAP LSHIFT ;
-: FSEL_MASK DUP DUP 2 + >R 1 + >R BILS 1 R> LSHIFT OR 1 R> LSHIFT OR ;
-: GPIO_LSB N_GPIO 10 MOD 3 * ;
-: FSEL GPIO_LSB DUP FSEL_MASK ;
-: GPFSEL N_GPIO 10 / 4 * GPFSEL0 + ;
-: GPSET N_GPIO 32 / 4 * GPSET0 + ;
-: GPCLR N_GPIO 32 / 4 * GPCLR0 + ;
-
-: GPLEV 32 / 4 * GPLEV0 + ;
-: PIN_LEVEL DUP GPLEV @ SWAP 32 MOD 1 SWAP LSHIFT AND IF 1 ELSE 0 THEN ;
-
+\\ 
+\\ Definizione di costanti legate al sensore in uso, tra cui umidità minima, umidità massima,
+\\ temperatura minima e temperatura massima registrabili
+\\ 
 -40                 CONSTANT MIN_TEMP
 80                  CONSTANT MAX_TEMP
 0                   CONSTANT MIN_HUM
 100                 CONSTANT MAX_HUM
-
+\\ 
+\\ Definizione di variabili usate per contenere i dati raccolti
+\\ 
+\\ Dati e checksum
 VARIABLE DATA
 VARIABLE CHECKSUM
-
+\\ 
+\\ Parti intere e parti decimali di umidità e temperatura
+\\ 
 VARIABLE HUMIDITY_IP
 VARIABLE HUMIDITY_DP
 VARIABLE TEMPERATURE_IP
 VARIABLE TEMPERATURE_DP
-
-24 FSEL_MASK    CONSTANT GPIO18_FSEL
-24 BILS         CONSTANT GPIO18_OUTPUT
-0               CONSTANT GPIO18_INPUT
-
-: WAIT_PULLDOWN BEGIN DUP PIN_LEVEL 0 = WHILE REPEAT DROP ;
-
-: WAIT_PULLUP BEGIN DUP PIN_LEVEL 1 = WHILE REPEAT DROP ;
-
-: SETUP_PIN OVER DUP FSEL -ROT MODE ROT GPFSEL ;
-
-: DHT_OUT GPIO18_FSEL GPIO18_OUTPUT GPFSEL1 ENABLE_PIN ;
-
-: DHT_INPUT GPIO18_FSEL GPIO18_INPUT GPFSEL1 ENABLE_PIN ;
-
+\\ 
+\\ Definizione di costanti legate alla connessione sensore-microcontrollore, nello specifico
+\\ maschera di FSEL per il pin in uso, valore da scrivere in GPFSEL per il pin in uso e per le 
+\\ modalità input e output e registro GPFSEL associato al pin in uso.
+\\ 
+GPIO18 FSEL          CONSTANT GPIO18_FSEL
+GPIO18 OUT MODE      CONSTANT GPIO18_OUT
+GPIO18 INP MODE      CONSTANT GPIO18_INP
+GPIO18 GPFSEL        CONSTANT GPIO18_GPFSEL
+\\
+\\ WAIT_PULLDOWN ( n_gpio --  )
+\\ Mantiene il sistema in busy-wait finché non viene rilevata una transizione da 1 a 0 nel
+\\ registro GPLEV e sul bit associati al pin cui è collegato il sensore
+\\ 
+: WAIT_PULLDOWN 
+    BEGIN 
+        DUP PIN_LEVEL 
+        0 = WHILE 
+    REPEAT 
+    DROP ;
+\\
+\\ WAIT_PULLUP ( n_gpio --  )
+\\ Mantiene il sistema in busy-wait finché non viene rilevata una transizione da 0 a 1 nel
+\\ registro GPLEV e sul bit associati al pin cui è collegato il sensore
+\\ 
+: WAIT_PULLUP 
+    BEGIN 
+        DUP PIN_LEVEL 
+        1 = WHILE 
+    REPEAT 
+    DROP ;
+\\  
+\\ DHT_PIN_OUT ( -- )
+\\ Scorciatoia per settare i parametri utili ad impostare la FSEL per il pin utilizzato in modalità output
+\\ 
+: DHT_PIN_OUT GPIO18_FSEL GPIO18_OUT GPIO18_GPFSEL ;
+\\ 
+\\ DHT_OUT ( -- )
+\\ Imposta il pin in modalità output
+\\ 
+: DHT_OUT DHT_PIN_OUT ENABLE_PIN ;
+\\  
+\\ DHT_PIN_INP ( -- )
+\\ Scorciatoia per settare i parametri utili ad impostare la FSEL per il pin utilizzato in modalità input
+\\ 
+: DHT_PIN_INP GPIO18_FSEL GPIO18_INP GPIO18_GPFSEL ;
+\\ 
+\\ DHT_INPUT ( -- )
+\\ Imposta il pin in modalità input
+\\ 
+: DHT_INPUT DHT_PIN_INP ENABLE_PIN ;
+\\ SETUP_SENSOR (  --  )
+\\ Esegue le istruzioni necessarie a mandare lo start signal al sensore, necessario al rilevamento da parte di
+\\ quest'ultimo. Nello specifico:
+\\ - imposta il pin in output mode
+\\ - attiva il bit associato al pin nel GPCLR opportuno e attende 1 ms
+\\ - attiva il bit associato al pin nel GPSET opportuno
+\\ - imposta il pin in input mode
+\\ 
 : SETUP_SENSOR 
     DHT_OUT
     GPIO18 DUP GPCLR !
-    1000 DELAY
+    1 MILLISECONDS CLK_DELAY
     GPIO18 DUP GPSET !
     DHT_INPUT ;
-
-: READ_BIT DUP WAIT_PULLDOWN CLO_REGISTER @ SWAP WAIT_PULLUP CLO_REGISTER @ SWAP - 50 > IF 1 ELSE 0 THEN ;
-
+\\ 
+\\ READ_BIT ( -- )
+\\ Viene usata per determinare se il sensore ha inviato al MCU uno 0 o un 1.
+\\ Calcoliamo la differenza tra il momento in cui avviene un pullup (fine dell'inizio trasmissione)
+\\ e quello, precedente, in cui è avvenuto un pulldown, che dev'essere almeno di 50 us, 
+\\ la soglia che permette di affermare se è stato trasmesso uno 0 o un 1.
+\\ 
+: READ_BIT DUP WAIT_PULLDOWN TIMER_COUNT @ SWAP WAIT_PULLUP TIMER_COUNT @ SWAP - 50 > IF 1 ELSE 0 THEN ;
+\\
+\\ READ_DATA ( -- )
+\\ Viene usata per effettuare la lettura di 40 bit per volta, conservando i primi 32 come dati effettivi nella
+\\ variabile DATA, mentre gli altri 8 saranno usati come checksum nella variabile omonima.
+\\ 
 : READ_DATA 
     GPIO18 N_GPIO DUP DUP DUP WAIT_PULLDOWN WAIT_PULLUP
     39 BEGIN
@@ -87,7 +110,10 @@ VARIABLE TEMPERATURE_DP
         OR SWAP !
         1 - DUP 0 >
     WHILE REPEAT 2DROP ;
-
+\\ 
+\\ GET_HUMIDITY ( -- )
+\\ Viene usata per ricavare la parte intera e la parte frazionaria dell'umidità dalla variabile DATA
+\\ 
 : GET_HUMIDITY 
     DATA @ 16 RSHIFT 10 /MOD DUP DUP MIN_HUM >= SWAP MAX_HUM <= AND
     IF
@@ -96,7 +122,10 @@ VARIABLE TEMPERATURE_DP
     ELSE
         2DROP
     THEN ;
-
+\\ 
+\\ GET_TEMPERATURE ( -- )
+\\ Viene usata per ricavare la parte intera e la parte frazionaria della temperatura dalla variabile DATA
+\\ 
 : GET_TEMPERATURE 
     DATA @ 65535 AND 10 /MOD DUP DUP MIN_TEMP >= SWAP MAX_TEMP <= AND
     IF
@@ -105,13 +134,29 @@ VARIABLE TEMPERATURE_DP
     ELSE
         2DROP
     THEN ;
-
+\\ 
+\\ GET_READING ( -- )
+\\ Parola comprensiva per ricavare i valori interi e decimali di temperatura e umidità
+\\ 
 : GET_READING GET_HUMIDITY GET_TEMPERATURE ;
-
-: HUMIDITY>CMD HUMIDITY_IP ? HUMIDITY_DP ? CR ;
-
-: TEMPERATURE>CMD TEMPERATURE_IP ? TEMPERATURE_DP ? CR ;
-
-: DHT>CMD TEMPERATURE>CMD HUMIDITY>CMD ;
-
+\\ 
+\\ HUMIDITY>CMD ( -- )
+\\ Parola usata per stampare su riga di comando il valore di umidità ricavato
+\\ 
+: HUMIDITY>CMD S" Humidity: " PRINT_STR HUMIDITY_IP ? S" . " PRINT_STR HUMIDITY_DP ? S" %" PRINT_STR ;
+\\ 
+\\ TEMPERATURE>CMD ( -- )
+\\ Parola usata per stampare su riga di comando il valore di temperatura ricavato
+\\ 
+: TEMPERATURE>CMD S" Temperature: " PRINT_STR TEMPERATURE_IP ? S" . " PRINT_STR TEMPERATURE_DP ? S" *C" PRINT_STR ;
+\\ 
+\\ DHT>CMD ( -- )
+\\ Parola comprensiva per stampare su riga di comando i valori di temperatura e di umidità ricavati
+\\ 
+: DHT>CMD TEMPERATURE>CMD S" -" PRINT_STR HUMIDITY>CMD CR ;
+\\ 
+\\ MEASURE ( -- )
+\\ Parola comprensiva per l'esecuzione dell'intero processo per una singola misurazione del sensore
+\\ 
 : MEASURE 0 DATA ! 0 CHECKSUM ! SETUP_SENSOR READ_DATA GET_READING DHT>CMD DROP ;
+\\ 
